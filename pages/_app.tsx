47dbdc64f7975efba31a378b3b1a98734cc2ad97
Daniel Merkl <daniel.merkl.dm@gmail.com>
2022-05-22 18:58:28 +0200

    init react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import "../styles/globals.css";
 
+const queryClient = new QueryClient();
+
 const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <QueryClientProvider client={queryClient}>
+    <Component {...pageProps} />
+  </QueryClientProvider>
 );
 
 export default App;
