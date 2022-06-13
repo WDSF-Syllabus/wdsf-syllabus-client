8d42bbe604bd38c04b0b5d9e013d2d09753a43df
Daniel Merkl <daniel.merkl.dm@gmail.com>
2022-06-13 13:43:29 +0200

    implement interface Figure

---
+++ model/interface/Figure.ts
@@ -0,0 +1,5 @@
+export interface Figure {
+  id: string;
+  name: string;
+  slug: string;
+}
--- utils/api.ts
+++ utils/api.ts
@@ -1,11 +1,33 @@
 import axios from "axios";
 
+import { flattenApiData } from "./flattenApiData";
+import { Figure } from "../model/interface/Figure";
+import { ApiResponse } from "../model/interface/ApiResponse";
+import { ApiData } from "../model/interface/ApiData";
+
 const apiUrl = process.env.API_URL;
 const token = process.env.API_TOKEN;
 
-export const api = axios.create({
+const http = axios.create({
   baseURL: apiUrl,
   headers: {
     Authorization: `Bearer ${token}`,
   },
 });
+
+async function getFigure(slug: string): Promise<Figure> {
+  const url = `/figures/${slug}`;
+  const response = await http.get<ApiResponse<ApiData<Figure>>>(url);
+  return flattenApiData(response.data.data);
+}
+
+async function getFigures(): Promise<Array<Figure>> {
+  const url = "/figures";
+  const response = await http.get<ApiResponse<Array<ApiData<Figure>>>>(url);
+  return response.data.data.map(flattenApiData);
+}
+
+export const api = {
+  getFigure,
+  getFigures,
+};
