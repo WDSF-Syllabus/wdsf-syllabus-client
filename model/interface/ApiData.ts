d840d8c5898c21833528c5dbe267c263a5f4185a
Daniel Merkl <daniel.merkl.dm@gmail.com>
2022-06-16 21:26:00 +0200

    🏷️ update interface ApiData with timestamps

--- model/interface/ApiData.ts
+++ model/interface/ApiData.ts
@@ -1,4 +1,10 @@
+interface Timestamps {
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+}
+
 export interface ApiData<T> {
   id: string;
-  attributes: Omit<T, "id">;
+  attributes: Omit<T, "id"> & Timestamps;
 }
