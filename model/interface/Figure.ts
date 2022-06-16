d7c45d06e69c3e2590d68bf945f56070caf8b210
Daniel Merkl <daniel.merkl.dm@gmail.com>
2022-06-16 16:30:56 +0200

    🏷️ missing properties of interface Figure

--- model/interface/Figure.ts
+++ model/interface/Figure.ts
@@ -1,5 +1,21 @@
+import { Dance } from "./Dance";
+import { Position } from "./Position";
+import { Timing } from "./Timing";
+import { Note } from "./Note";
+import { FootPlacement } from "./FootPlacement";
+import { Foot } from "./Foot";
+
 export interface Figure {
   id: string;
+  dance: Dance;
   name: string;
+  startPositions: Array<Position>;
+  endPositions: Array<Position>;
+  timings: Array<Timing>;
+  notes: Array<Note>;
   slug: string;
+  startFootPlacements: Array<FootPlacement>;
+  endFootPlacements: Array<FootPlacement>;
+  startFoot: Foot;
+  endFoot: Foot;
 }
