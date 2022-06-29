f67baf7171784fe52b8097b084605dc9cb7857c4
DanielMerkl <daniel.merkl.dm@gmail.com>
2022-06-29 13:06:19 +0200

    🔧 add redirect from root to /figures

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: () => [
+    {
+      source: "/",
+      destination: "/figures",
+      permanent: false,
+    },
+  ],
 };
 
 module.exports = nextConfig;
